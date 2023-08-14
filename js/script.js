4ad5cff6632db82033bbca6094955049804218ee
kohi9noor <kohinoornimes@gmail.com>
2023-08-14 12:23:10 +0530

    CREATED MAIN LOOP

--- js/script.js
+++ js/script.js
@@ -1,57 +1,43 @@
-import check from'../assets/check.svg'
-import star from'../assets/star.svg'
-import sushi12 from '../assets/sushi-12.png'
-import sushi11 from '../assets/sushi-11.png'
-import sushi10 from '../assets/sushi-10.png'
+var tl = gsap.timeline()
 
-import AOS from "aos";
 
-import "aos/dist/aos.css";
+tl.from(".header__logo h4",{
+    y:-100,
+    duration:1,
+    delay:1,
+    opacity:0
+})
 
-AOS.init({
-    duration: 1000,
-    offset: 100,
-});
+tl.from(".header__nav ul", { 
+    duration:1,
+    opacity:0,
+})
 
-const trendingSushis = [
-    'Make Sushi',
-    'Nigiri Sushi',
-    'Oshizushi',
-    'Temaki Sushi',
-    'Uramaki Sushi',
-    'Inari Sushi'
-];
+tl.from(".hero-image img",{
+    duration:1,
+    opacity:0,
+})
 
-const trendingDrinks = [
-    "Oruncha",
-    "Ofukucha",
-    "Sakura Tea",
-    "Kombu-cha",
-    "Aojiru",
-    "Mugicha",
-]
+tl.from(".hero-content h1 ", {
+    x:100,
+    duration:1,
+    opacity:0
+})
 
-const cards = [
-    {
-        imgSrc: sushi12,
-        alt: "sushi-12",
-        title: "Chezu Sushi",
-        rating: "4.8",
-        price: "$21.00"
-    },
-    {
-        imgSrc: sushi11,
-        alt: "sushi-11",
-        title: "Originale Sushi",
-        rating: "4.8",
-        price: "$21.00",
-        active: true
-    },
-    {
-        imgSrc: sushi10,
-        alt: "sushi-10",
-        title: "Ramen Legendo",
-        rating: "4.8",
-        price: "$21.00"
-    }
-];
+tl.from(".hero-content p", {
+    x:100,
+    duration:1,
+    opacity:0
+})
+
+tl.from(".about-us__image img", {
+    x:-100,
+    duration:1,
+    opacity:0
+})
+
+tl.from(".about-us__content h3",{
+    duration1,
+    y:100,
+    opacity:0
+})
